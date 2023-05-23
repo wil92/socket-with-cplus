00c5fa2f960b6f0956e7b87279de3db2c2b99b39
Guillermo <ggjnez92@gmail.com>
2023-05-23 23:03:14 +0200

    Successful communication between server and client

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
+#include <cstring>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 #define BUFFER_LENGTH 20
 
@@ -11,23 +15,48 @@
 int portno = 4333;
 
 int iteMax = 10, ite = 0;
+
 bool iterate() {
     return ite++ < iteMax;
 }
 
 #ifdef SERVER
 #define LISTEN_BACKLOG 50
+#define MAX_DATA_SIZE 100
+
+std::vector<pthread_t> connections;
+
+void *handleConnection(void *args) {
+    int *sockFd = (int *) args;
+    std::cout << "connected to client with socket " << *sockFd << std::endl;
+
+    char buff[MAX_DATA_SIZE];
+    ssize_t numBytes;
+    while (true) {
+        if ((numBytes = recv(*sockFd, buff, MAX_DATA_SIZE - 1, 0)) == -1) {
+            handle_error("receiving message");
+        }
+        if (numBytes == 0) {
+            break;
+        }
+        buff[numBytes] = '\0';
+        std::cout << "Connection: " << *sockFd << ", Message: " << buff << std::endl;
+    }
+
+    return nullptr;
+}
 
 int main() {
     std::cout << "Server started" << std::endl;
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
+    struct sockaddr_in addr{};
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(portno);
     addr.sin_addr.s_addr = INADDR_ANY;
+    // depending on the system modify the function to call (in mac is different)
+    addr.sin_port = htons(portno);
 
     if (bind(sockfd, (sockaddr *) &addr, sizeof(addr)) == -1) {
         handle_error("bind");
@@ -40,27 +69,24 @@ int main() {
 //    int bufferLen = BUFFER_LENGTH;
 //    int buffer[BUFFER_LENGTH];
 
-//    while (true) {
-    struct sockaddr_storage addrNew;
-    {
+    struct sockaddr_storage addrNew{};
+    std::vector<int> socks;
+    while (true) {
         // accept connections
         socklen_t addrNewSize = sizeof(addrNew);
         int socknew = accept(sockfd, (sockaddr *) &addrNew, &addrNewSize);
         if (socknew == -1) {
             handle_error("accept");
         }
-        char tt[] = "hello";
-        send(socknew, &tt, sizeof(tt), 0);
-//        ssize_t res = recv(sockfd, buffer, bufferLen, 0);
-//        std::cout<<res<<std::endl;
-//        if (res != -1) {
-//            exit(1);
-//        }
-//        if(!iterate()) {
-//            break;
-//        }
+        socks.push_back(socknew);
+        pthread_t newThread = pthread_t();
+        pthread_create(&newThread, nullptr, &handleConnection, &socknew);
+        connections.push_back(newThread);
     }
 
+    for (int i = 0; i < socks.size(); ++i) {
+        close(socks[i]);
+    }
     return 0;
 }
 #else
@@ -74,21 +100,23 @@ int main() {
     addr.sin_family = AF_INET;
     addr.sin_port = htons(portno);
     addr.sin_addr.s_addr = INADDR_ANY;
+//    addr.sin_addr.s_addr = inet_addr("207.180.211.97");
 
     if (connect(sockfd, (sockaddr *) &addr, sizeof(addr)) == -1) {
         handle_error("connect");
     }
 
-//    while (true) {
+    while (true)
     {
-        char buffer[20];
-        size_t bufferSize = sizeof(buffer);
-        ssize_t res = recv(sockfd, &buffer, bufferSize, 0);
+        char* buffer = "Hello world!!!";
+        size_t bufferSize = strlen(buffer);
+        ssize_t res = send(sockfd, buffer, bufferSize, 0);
         std::cout<<res<<std::endl;
         if (res == -1) {
             handle_error("recv");
         }
         std::cout<<buffer<<std::endl;
+        sleep(1);
     }
 }
 #endif
